6b5a12599b6dc43490bcb95bf0e310006854c365
josePereiro <jose.pereiro.lab@gmail.com>
2021-03-30 22:07:23 -0400

    up plots

--- scripts/iJR904/2.2.3_var_study.jl
+++ scripts/iJR904/2.2.3_var_study.jl
@@ -1,36 +1,23 @@
 let
     method = ME_MAX_POL
+    niders = length(FLX_IDERS)
 
-    var_avs = Dict()
-    var_stds = Dict()
-    for exp in EXPS
-        datfile = INDEX[method, :DFILE, exp]
-        dat = deserialize(datfile)
-        model = dat[:model]
-        idxs = map(Fd.msd_mets) do Fd_ider
-            ChU.rxnindex(model, Fd_rxns_map[Fd_ider])
-        end
-        
-        epouts = dat[:epouts]
-        exp_beta = maximum(keys(epouts))
-        epout = epouts[exp_beta]
-        
-        vas = ChU.va(epout)[idxs]
-        var_avs[exp] = mean(vas)
-        var_stds[exp] = 0.0
+    stds = map(EXPS) do exp
+        DAT[method, :eperr, :flx, FLX_IDERS, exp] ./ abs.(DAT[method, :Fd, :flx, "GLC", exp])
     end
-
+    
     # sGLC
     ids = [:D, :X, :uAC, :uGLC, :uLAC, :uPYR]
     for id in ids
-        p = scatter(;xlabel = string(id), ylabel = "avs var")
-        xs = [Fd.val(id, exp) for exp in EXPS]
-        ys = [var_avs[exp] for exp in EXPS]
-        errs = [var_stds[exp] for exp in EXPS]
-        sids = sortperm(xs)
-        xs, ys, errs = xs[sids], ys[sids], errs[sids]
-        scatter!(p, xs, ys; yerr = errs, label = "", color = :black, m = 8)
-        plot!(p, xs, ys; label = "", color = :black, lw = 3, alpha = 0.5)
-        mysavefig(p, string("av_var_vs_", id))
+        p = scatter(;xlabel = string(id), ylabel = "std")
+        id_vals = [Fd.val(id, exp) for exp in EXPS]
+        sidx = sortperm(id_vals)
+        for idx in sidx
+            x = id_vals[idx]
+            ys = stds[idx]
+            scatter!(p, fill(x, niders), ys; label = "", color = :black, m = 8)
+            scatter!(p, [x], [mean(ys)]; label = "", color = :red, m = (8, :star))
+        end
+        mysavefig(p, "av_var_vs_"; id)
     end
-end
+end    
